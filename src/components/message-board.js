42cc39b49aa5dfadaa0be90e75f168e7d7a7a713
Darian Morrison <darianjmorrison@gmail.com>
2023-06-15 18:27:12 -0700

    Added readOnly Behaviour.

--- src/components/message-board.js
+++ src/components/message-board.js
@@ -1,14 +1,33 @@
+"use client";
+import { useState } from "react";
+
 import MessageForm from "@/components/message-form";
 import MessageItem from "@/components/message-item";
 
 export default function MessageBoard({ messages }) {
+  const [readOnly, setReadOnly] = useState(false);
+  const handleReadOnlyChange = (event) => {
+    setReadOnly(event.target.checked);
+  };
+
   return (
     <div className="w-11/12 md:max-w-none p-6 mx-6 bg-white rounded-xl shadow-md">
       <h1 className="text-center text-3xl font-bold mb-4">Our Message Board</h1>
       {messages.map((message) => (
-        <MessageItem key={message.id} message={message} />
+        <MessageItem key={message.id} message={message} readOnly={readOnly} />
       ))}
-      <MessageForm />
+      {!readOnly && <MessageForm />}
+      <div className="flex items-center justify-end">
+        <input
+          type="checkbox"
+          id="hideForm"
+          checked={readOnly}
+          onChange={handleReadOnlyChange}
+        />
+        <label htmlFor="hideForm" className="ml-2 text-xm-sm">
+          Read Only
+        </label>
+      </div>
     </div>
   );
 }
--- src/components/message-item.js
+++ src/components/message-item.js
@@ -5,7 +5,7 @@ import { formatRelative } from "date-fns";
 import { capitalizeFirstLetter } from "@/utils/string-helpers";
 import { deleteMessage } from "@/utils/actions/message-actions";
 
-export default function MessageItem({ message }) {
+export default function MessageItem({ message, readOnly }) {
   const router = useRouter();
 
   const handleDeleteMessage = async (id) => {
@@ -20,16 +20,18 @@ export default function MessageItem({ message }) {
     >
       <div className="whitespace-pre-wrap">{message.text}</div>
       <hr className="m-2" />
-      <div className="flex flex-row justify-between">
-        <p className="text-xs text-gray-500">
+      <div className="flex flex-row justify-end">
+        <p className="pr-1 text-xs text-gray-500">
           {capitalizeFirstLetter(formatRelative(message.createdAt, new Date()))}
         </p>
-        <button
-          onClick={() => handleDeleteMessage(message.id)}
-          className="text-red-500"
-        >
-          <AiOutlineClose size={20} />
-        </button>
+        {!readOnly && (
+          <button
+            onClick={() => handleDeleteMessage(message.id)}
+            className="text-red-500"
+          >
+            <AiOutlineClose size={20} />
+          </button>
+        )}
       </div>
     </div>
   );
