a74657d8a497ae1a4991e2c54d8e84b3eb8dbcfe
Darian Morrison <darianjmorrison@gmail.com>
2023-07-07 21:27:02 -0700

    Moved components into app and add messages to bottom of page now.

--- src/app/message-board.tsx
+++ src/app/message-board.tsx
@@ -1,22 +1,19 @@
 "use client";
 import { useState } from "react";
-import { Message } from '@prisma/client'
+import { Message } from "@prisma/client";
 
-import MessageForm from "@/components/message-form";
-import MessageItem from "@/components/message-item";
+import MessageForm from "@/app/message-form";
+import MessageItem from "@/app/message-item";
 
-export default function MessageBoard({ messages }: {messages: Message[] }){
+export default function MessageBoard({ messages }: { messages: Message[] }) {
   const [readOnly, setReadOnly] = useState(false);
-  const handleReadOnlyChange = (event: React.ChangeEvent<HTMLInputElement> ) => {
+  const handleReadOnlyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setReadOnly(event?.target?.checked || false);
   };
 
   return (
     <div className="w-11/12 md:max-w-none p-6 mx-6 bg-white rounded-xl shadow-md">
       <h1 className="text-center text-3xl font-bold mb-4">Our Message Board</h1>
-      {messages.map((message) => (
-        <MessageItem key={message.id} message={message} readOnly={readOnly} />
-      ))}
       {!readOnly && <MessageForm />}
       <div className="flex items-center justify-end">
         <input
@@ -29,6 +26,9 @@ export default function MessageBoard({ messages }: {messages: Message[] }){
           Read Only
         </label>
       </div>
+      {messages.map((message) => (
+        <MessageItem key={message.id} message={message} readOnly={readOnly} />
+      ))}
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import MessageBoard from "@/components/message-board";
+import MessageBoard from "@/app/message-board";
 
 import prisma from "@/utils/primsa";
 
 export default async function Home() {
-  const messages = await prisma.message.findMany();
+  const messages = await prisma.message.findMany({
+    orderBy: [
+      {
+        createdAt: "desc",
+      },
+    ],
+  });
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-blue-100">
       <MessageBoard messages={messages} />
